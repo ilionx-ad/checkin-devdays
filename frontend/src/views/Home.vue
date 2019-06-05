9336d5cd66150264d335b4c4a4b34f2088bbae32
Jeroen Burgers <jburgers@ilionx.com>
2019-06-05 19:54:19 +0200

    feat (building check in page)

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welkom bij de<br />ilionx DevDays 2019</h1>
+    <h2>Laat weten dat je er bent!</h2>
+    <a class="button">Check nu in!</a>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+  h1, h2 {
+    margin: 0;
   }
+
+  h2 {
+    color: $color-secondary;
+    font-size: 60px;
+  }
+</style>
+
+
+<script>
+export default {
+  name: 'home'
 }
 </script>
