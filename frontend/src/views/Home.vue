4bfbc91f69e69b6abb510e96432d1b68a6d1585a
Mike Timmerman <mike.timmerman@qnh.nl>
2019-07-10 19:43:55 +0200

    feat: add guid on dialog start

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,28 +1,41 @@
+
+
 <template>
   <div class="home">
-    <h1>Welkom bij de<br />ilionx DevDays 2019</h1>
+    <h1>
+      Welkom bij de
+      <br />ilionx DevDays 2019
+    </h1>
     <h2>Laat weten dat je er bent!</h2>
-    <a class="button">Check nu in!</a>
+    <a class="button" @click="setIdInStorage()">Check nu in!</a>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .home {
-    align-self: center;
-  }
-  h1, h2 {
-    margin: 0;
-  }
+.home {
+  align-self: center;
+}
+h1,
+h2 {
+  margin: 0;
+}
 
-  h2 {
-    color: $color-secondary;
-    font-size: 60px;
-  }
+h2 {
+  color: $color-secondary;
+  font-size: 60px;
+}
 </style>
 
 
 <script>
+import * as uuidv1 from "uuid/v1";
+
 export default {
-  name: 'home'
-}
+  name: "home",
+  methods: {
+    setIdInStorage: () => {
+      sessionStorage.setItem("endBossId", uuidv1());
+    }
+  }
+};
 </script>
