5e6eb6328f20df891edec67caaca54c93e04a3a9
Mike Timmerman <mike.timmerman@qnh.nl>
2019-09-06 15:56:11 +0200

    changes

---
+++ frontend/src/components/Chat/Chat.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div v-for="item in this.$store.getters.all" v-bind:key="item.id">
+      <div class="wrapper">
+        <div class="balloon">{{item.question}}</div>
+      </div>
+      <div class="wrapper right">
+        <div class="balloon green">{{item.answer}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat"
+};
+</script>
+
+<style scoped lang="scss">
+.balloon {
+  border-radius: 20px;
+  line-height: 20px;
+  display: inline-block;
+  background: blue;
+  padding: 10px;
+  color: white;
+}
+
+.balloon.green {
+  background: green;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 15px;
+  align-items: flex-start;
+}
+
+.wrapper.right {
+  align-items: flex-end;
+}
+</style>
